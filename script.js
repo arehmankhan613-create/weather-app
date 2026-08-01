48405fe0bfef8987fe2fdd90f19f786cc3f30d32
arehmankhan613-create <arehmankhan613@gmail.com>
2026-08-01 20:44:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
-function getWeather(){
+const apiKey = "abef934d25f6dec0ca4bc6318ee37056";
 
-let city=document.getElementById("city").value;
+async function getWeather() {
 
-document.getElementById("result").innerHTML=
-"Searching weather for <b>"+city+"</b>";
+    const city = document.getElementById("city").value;
+
+    if (city === "") {
+        alert("Please enter a city name");
+        return;
+    }
+
+    document.getElementById("loading").innerHTML = "Loading...";
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        document.getElementById("loading").innerHTML = "";
+
+        if (data.cod != 200) {
+            document.getElementById("result").innerHTML =
+                "<h3>❌ City Not Found</h3>";
+            return;
+        }
+
+        document.getElementById("result").innerHTML = `
+            <h2>${data.name}, ${data.sys.country}</h2>
+
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+
+            <h1>${data.main.temp}°C</h1>
+
+            <p><b>${data.weather[0].description}</b></p>
+
+            <p>🤗 Feels Like : ${data.main.feels_like}°C</p>
+
+            <p>💧 Humidity : ${data.main.humidity}%</p>
+
+            <p>💨 Wind : ${data.wind.speed} m/s</p>
+
+            <p>🌍 Pressure : ${data.main.pressure} hPa</p>
+        `;
+
+    } catch (error) {
+
+        document.getElementById("loading").innerHTML = "";
+        document.getElementById("result").innerHTML = "Something went wrong.";
+
+    }
 
 }
